5dc80e9fbaae5affc95c4268ad94c18a01074fe3
KellyAllen013069 <89815777+KellyAllen013069@users.noreply.github.com>
2024-07-17 12:05:55 -0500

    Student class created

---
+++ Module2Review/Student.cs
@@ -0,0 +1,19 @@
+namespace Module2Review;
+
+public class Student
+{
+    public string Name { get; set; }
+    public int ID { get; set; }
+    public List<double> Grades { get; set; } = new List<double>() {3.2, 3.9, 2.3, 2.9, 1.67};
+
+    public void AddGrade(double grade)
+    {
+        Grades.Add(grade);
+    }
+
+    public void AddGrade(params double[] grades)
+    {
+        Grades.AddRange(grades);
+    }
+    
+}
